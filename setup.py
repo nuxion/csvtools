8d166deef88831dbb02b1ec5ed9bf15ab0cba0dd
nuxion <nuxion@gmail.com>
2018-09-12 16:34:54 -0300

    writer: packaging

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="csvtoolz",
-    version="0.0.4",
+    version="0.0.5",
     author="Xavier Petit",
     author_email="nuxion@gmail.com",
     description="Manage CSV related tasks",
